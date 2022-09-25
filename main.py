7a4bd7d7eea95221a63386c7f2b641f464d799cd
Krushil Naik <krushilnaik96@gmail.com>
2022-09-25 19:57:36 -0400

    Update .gitignore, pull data from API

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-#! ./venv/bin/python
+#! ./venv/Scripts/python
 
 # INF601 - Advanced Programming in Python
 # Krushil Naik
 # Mini Project 2
 
 # (5/5 points) Initial comments with your name, class and project at the top of your .py file.
-# (5/5 points) Proper import of packages used.
 # (20/20 points) Using a data source of your choice, such as data from data.gov or using the Faker package, generate or retrieve some data for creating basic statistics on. This will generally come in as json data, etc.
 # Think of some question you would like to solve such as:
 # "How many homes in the US have access to 100Mbps Internet or more?"
@@ -17,3 +16,24 @@
 # (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
 # (10/10 points) I will be checking out the master branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
 # (20/20 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown. Be thorough on the explanations.
+
+# (5/5 points) Proper import of packages used.
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import requests
+
+
+if not os.path.exists("./charts"):
+    os.mkdir("./charts")
+
+city = "Tokyo"
+
+# I'm aware this is unsafe, but unsure how this would be graded without it
+API_KEY = "P6nDiEWzEias+qnnrMReaw==hAaNKc72nVArU8DT"
+
+# Gather air quality data from the cities in the cities list
+response = requests.get(
+    f"https://api.api-ninjas.com/v1/airquality?{city=}",
+    headers={"X-Api-Key": API_KEY},
+)
